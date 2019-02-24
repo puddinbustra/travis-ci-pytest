0950be6a95b585ff2bc7a9669c69c434eea817b3
puddinbustra <43453214+puddinbustra@users.noreply.github.com>
2019-02-23 21:58:27 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from glob import glob
 
 from setuptools import find_packages
 
-# setup(name='Fibonacci',
-#       version='1.0',
-#       description='Python Distribution Utilities',
-#       author='Kevin Chen',
-#       packages=find_packages('src'),
-#       package_dir={'': 'src'},
-#       #py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
-#      )
+setup(name='Fibonacci',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Kevin Chen',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      #py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+     )
