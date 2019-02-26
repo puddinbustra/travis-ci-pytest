281676c3bc59125df339867c6a3108eadb61aff1
puddinbustra <43453214+puddinbustra@users.noreply.github.com>
2019-02-25 18:29:36 -0700

    Update test_fib2.py

--- tests/test_fib2.py
+++ tests/test_fib2.py
@@ -4,4 +4,4 @@ from mypkg.fibonacci import fibonacci
 
 def test_fib_10():
 	assert(fibonacci(10) == 551)
-print("This is just a test to see if it goes through multiple automatcially)
+print("This is just a test to see if it goes through multiple automatcially")
