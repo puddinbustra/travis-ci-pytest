bf9d3d66214d3458c377a4ca10291764cf8c6742
puddinbustra <43453214+puddinbustra@users.noreply.github.com>
2019-02-25 18:20:25 -0700

    Update justTest.py

--- tests/justTest.py
+++ tests/justTest.py
@@ -3,4 +3,4 @@ print("This is just a test to see if it goes through multiple automatcially)
 from mypkg.fibonacci import fibonacci
 
 def test_fib_10():
-	assert(fibonacci(10) == 550)
+	assert(fibonacci(10) == 551)
