9f3877eb2cbf7587cdcf286b90ffdca881efd44c
kinkalo <kvrosevelt@gmail.com>
2019-02-26 12:16:23 -0700

    added happy

---
+++ src/mypkg/happy.py
@@ -0,0 +1,13 @@
+
+
+def happy(input):
+    if int(input) == 10:
+        print("Happiness is here")
+        return 55
+    elif int(input) == 55:
+        try:
+            return 55
+        except:
+            print("I found an error, but I don't care")
+    else:
+        print("There is only sadness")
--- tests/test_fib.py
+++ tests/test_fib.py
@@ -1,5 +1,6 @@
 import pytest
 from mypkg.fibonacci import fibonacci
+from mypkg.happy import happy
 
 
 def test_fib_10():
