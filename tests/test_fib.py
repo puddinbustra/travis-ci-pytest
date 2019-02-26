2c65fad1939364e9337ac986a025b7e18ff62ed8
puddinbustra <43453214+puddinbustra@users.noreply.github.com>
2019-02-25 18:13:50 -0700

    Update test_fib.py

--- tests/test_fib.py
+++ tests/test_fib.py
@@ -3,7 +3,7 @@ import pytest
 from mypkg.fibonacci import fibonacci
 
 def test_fib_10():
-	assert(fibonacci(10) == 550)
+	assert(fibonacci(10) == 55)
 
 def test_fib_not_20():
 	assert(fibonacci(20) != 20)	
