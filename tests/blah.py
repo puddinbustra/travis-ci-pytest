de405ff2aceccdf59f20660121a5a747004892a7
puddinbustra <43453214+puddinbustra@users.noreply.github.com>
2019-02-25 19:18:54 -0700

    Update blah.py

--- tests/blah.py
+++ tests/blah.py
@@ -1,6 +1,7 @@
 import pytest
 
-from mypkg.fibonacci import fibonacci
+#from mypkg.fibonacci import fibonacci
+from src.fibonacci import fibonacci
 
 def test_fib_10():
 	assert(fibonacci(10) == 55)
