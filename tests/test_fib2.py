59be0edd049a4a30f13d57320d6303d84f96c34b
kinkalo <kvrosevelt@gmail.com>
2019-02-25 21:22:03 -0700

    Create test_fib2.py

---
+++ tests/test_fib2.py
@@ -0,0 +1,11 @@
+import pytest
+#from src.fibonacci import fibonacci
+from mypkg.fibonacci import fibonacci
+
+
+
+def test_fib_10():
+	assert(fibonacci(10) == 10)
+
+def test_fib_not_20():
+	assert(fibonacci(20) != 20)	
