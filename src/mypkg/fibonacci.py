8f74f7b89572739d4d628c841eed0c8745ca4dda
puddinbustra <43453214+puddinbustra@users.noreply.github.com>
2019-02-25 20:59:06 -0700

    Update fibonacci.py

--- src/mypkg/fibonacci.py
+++ src/mypkg/fibonacci.py
@@ -4,9 +4,20 @@
 Fibonacci function
 """
 
-def fibonacci(n, a=0, b=1):
-	if n == 0: # edge case
-		return a
-	if n == 1: # usual base case
-		return b
-	return fibonacci(n-1, b, a+b)
+# def fibonacci(n, a=0, b=1):
+# 	if n == 0: # edge case
+# 		return a
+# 	if n == 1: # usual base case
+# 		return b
+# 	return fibonacci(n-1, b, a+b)
+def fibonacci(input):
+    if int(input) == 10:
+        print("Happiness is here")
+        return 10
+    elif int(input) == 9:
+        try:
+            cake(3,5,4,6)
+        except:
+            print("I found an error, but I don't care")
+    else:
+        print("There is only sadness")
